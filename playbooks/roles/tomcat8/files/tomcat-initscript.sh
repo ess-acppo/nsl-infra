fc1a4a665b8cd4d8343769b03e8444e01cd7cafa
moziauddin1 <mohammad.ziauddin@agriculture.gov.au>
2018-07-19 09:14:48 +1000

    updated java home in tomcat initscript

--- playbooks/roles/tomcat8/files/tomcat-initscript.sh
+++ playbooks/roles/tomcat8/files/tomcat-initscript.sh
@@ -15,7 +15,7 @@
 #
  
 #Location of JAVA_HOME (bin files)
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/opt/jdk
  
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH
